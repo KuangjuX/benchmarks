d004bca3785fd63c6a7277e5effb2d18c92ed107
KuangjuX <18630816527@163.com>
2024-11-13 01:52:00 +0000

    backup

--- benchs/fused_gemm/tiledcuda/entry.py
+++ benchs/fused_gemm/tiledcuda/entry.py
@@ -12,53 +12,76 @@ static constexpr int kTM = {kTM};
 static constexpr int kTN = {kTN};
 static constexpr int kTK = {kTK};
 static constexpr int kTP = {kTP};
-
-static constexpr int kRK = {kRK};
 """
 
 kernel_entry = """
 extern "C" int kernel_entry(const __half* A, const __half* B, const __half* C, float* D) {
+
+    static_assert(kK == kTK, "The current implementation requires kTK == K.");
+    static_assert(kP == kTP, "The current implementation requires kTP == P.");
+
     using InType = __half;
     using AccType = float;
 
-    using WholeShape = GemmShape<kM, kN, kK>;
-    using CtaTileShape = GemmShape<kTM, kTN, kTK>;
+    using WholeShape = FusedGemmShape<kM, kN, kK, kP>;
+    using CtaTileShape = FusedGemmShape<kTM, kTN, kTK, kTP>;
     using WarpLayout = tl::RowMajor<kWarpPerRow, kWarpPerCol>;
 
-    using Config = KeGemmTraits<InType, AccType, WholeShape, CtaTileShape, kRK,
-                                WarpLayout>;
+    using Config = FusedGemmTraits<InType, AccType, WholeShape, CtaTileShape, WarpLayout>;
 
-    auto kernel =
-        &gemm<InType, AccType, kM, kN, kK, kTM, kTN, kTK,
-              typename Config::GIteratorA, typename Config::SIteratorA,
-              typename Config::SharedA, typename Config::RegA,
-              typename Config::G2SLoaderA, typename Config::S2RLoaderA,
-              typename Config::GIteratorB, typename Config::SIteratorB,
-              typename Config::SharedB, typename Config::RegB,
-              typename Config::G2SLoaderB, typename Config::S2RLoaderB,
-              typename Config::GlobalC, typename Config::SharedC,
-              typename Config::RegC, typename Config::R2SStorerC,
-              typename Config::S2GStorerC>;
+    using RegA = typename Config::RegA;
+    using RegB = typename Config::RegB;
+    using RegC = typename Config::RegC;
+    using RegD = typename Config::RegD;
+    using RegAcc = typename Config::RegAcc;
+    using RegAccCast = typename Config::RegAccCast;
 
-    static constexpr int smem_size_inputs = kTK * (kTN + kTM) * sizeof(InType);
-    static constexpr int smem_size_accumulators = kTM * kTN * sizeof(AccType);
-    static constexpr int smem_size = smem_size_inputs > smem_size_accumulators
-                                         ? smem_size_inputs
-                                         : smem_size_accumulators;
+    using GIteratorA = typename Config::GIteratorA;
+    using SharedA = typename Config::SharedA;
+    using SharedALoader = typename Config::SharedALoader;
+    using RegALoader = typename Config::RegALoader;
 
-    const int kMaxSmemPerBlock = 48 * 1024;
-    if (smem_size > kMaxSmemPerBlock) {
+    using GIteratorB = typename Config::GIteratorB;
+    using SharedB = typename Config::SharedB;
+    using SharedBLoader = typename Config::SharedBLoader;
+    using RegBLoader = typename Config::RegBLoader;
+
+    using GIteratorC = typename Config::GIteratorC;
+    using SharedC = typename Config::SharedC;
+    using SharedCLoader = typename Config::SharedCLoader;
+    using RegCLoader = typename Config::RegCLoader;
+
+    using DStorer = typename Config::DStorer;
+
+    using ConvertAcc = typename Config::ConvertHalf;
+
+    auto kernel = &KeFusedGemm<InType, AccType,            //
+                               GIteratorA, SharedA, RegA,  //
+                               SharedALoader, RegALoader,  //
+                               GIteratorB, SharedB, RegB,  //
+                               SharedBLoader, RegBLoader,  //
+                               GIteratorC, SharedC, RegC,  //
+                               SharedCLoader, RegCLoader,  //
+                               RegAcc, RegAccCast, typename Config::GlobalD,
+                               RegD, DStorer, ConvertAcc>;
+
+    int shm_input = (kTM * kTK + kTK * kTN + kTN * kTP);
+    int shm_output = kTM * kTP;
+    int shm_size = shm_input < shm_output ? shm_output * sizeof(InType)
+                                          : shm_input * sizeof(InType);
+
+    if (shm_size > 48 * 1024) {
         cudaFuncSetAttribute(
-            kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, smem_size);
+            kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, shm_size);
     }
 
     int block_x = CeilDiv<kM, kTM>;
-    int block_y = CeilDiv<kN, kTN>;
+    int block_y = CeilDiv<kP, kTP>;
 
-    dim3 dim_grid(block_x, block_y, 1);
-    dim3 dim_block(Config::kThreads, 1, 1);
+    dim3 grid(block_x, block_y, 1);
+    dim3 block(Config::kThreads, 1, 1);
 
-    kernel<<<dim_grid, dim_block, smem_size>>>(A, B, C);
+    kernel<<<dim_grid, dim_block, smem_size>>>(A, B, C, D, kM, kN, kK, kP, kTM, kTN, kTK, kTP);
     
     return 0;
 }
